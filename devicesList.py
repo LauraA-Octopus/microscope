e69871b47945ee558ab5fba91ba99ea85196501d
LauraA-Octopus <laura.arcidiacono@stfc.ac.uk>
2024-08-01 09:17:10 +0100

    AMC300 fully integrated in microscope and cockpit. tested.

--- devicesList.py
+++ devicesList.py
@@ -5,9 +5,10 @@ from microscope.stages.AMC300 import AMC300Adapter
 
 
 DEVICES = [
-    device(AMC300Adapter,"192.168.0.2", 9090, conf={"ip":"192.168.0.2", "port":9090}),
-    #device(PVCamera, "localhost", 1, uid= "A19J203005"),
-    #device(PVCamera, "localhost", 2, uid= "A19F203039"),
+    
+    device(PVCamera, "localhost", 1, uid= "A19J203005"),
+    device(PVCamera, "localhost", 2, uid= "A19F203039"),
     #device(ThorlabsFilterWheel, "localhost", 65130, conf={"com": "COM5", "baud": 115200, "timeout": 1}),
-    #device(ThorlabsFilterWheel, "localhost", 65131, conf={"com": "COM10", "baud": 115200, "timeout": 1}),
+    device(ThorlabsFilterWheel, "localhost", 65131, conf={"com": "COM10", "baud": 115200, "timeout": 1}),
+    device(AMC300Adapter, "localhost", 3, conf={"ip":"192.168.0.2", "port":9090, "x_limits": (100000, 5900000), "y_limits": (100000, 5900000), "z_limits": (100000, 5000000)}),
 ]
--- microscope/stages/AMC300.py
+++ microscope/stages/AMC300.py
@@ -1,147 +1,304 @@
 import os
+import typing
 import time
 import microscope
 import microscope.abc
 import microscope._utils
+from microscope.stages.AMCsoft.AMC import Device as AMCDevice
 
-#AMC_files = 'C:\\Users\\LSFMICRO\\Documents\\microscope\\microscope\\stages\\AMCsoft\\AMC.py'
-#os.path.realpath(AMC_files)
-from microscope.stages.AMCsoft.AMC import *
- 
+class AMC300Axis(microscope.abc.StageAxis):
+
+    def __init__(self, amc, index, limits: tuple[float, float], timeout:float):
+        self._amc = amc
+        self._index = index
+        self._limits = microscope.AxisLimits(*limits)
+        self._timeout = timeout
+        super().__init__()
+
+    def move_by(self, delta: float) -> None:
+        """Move axis by given amount."""
+        self.move_to(self.position + delta)
+
+    def move_to(self, pos: float) -> None:
+        """Move axis to specified position."""
+        self._amc.move.setControlTargetPosition(self._index, pos)
+        self._amc.control.setControlMove(self._index, True)
+
+
+    @property
+    def position(self) -> float:
+        """Current axis position."""
+        return self._amc.move.getPosition(self._index)
+
+    @property
+    def limits(self) -> microscope.AxisLimits:
+        """Upper and lower limits values for position."""
+        return self._limits
+
+    def wait(self):
+        timeout = self._timeout + time.time()
+        while self._amc.control.getStatusMovingAllAxes()[self._index] and time.time() < timeout:
+            time.sleep(0.1)
+
 class AMC300Adapter(microscope.abc.Stage):
-    def __init__(self, ip, port, **kwargs):
-        super().__init__(**kwargs)
+
+    def __init__(self, ip, port, x_limits, y_limits, z_limits, xyz=(0, 1, 2), timeout:float=30, **kwargs):
+        super().__init__()
         self.ip = ip
         self.port = port
-        self.amc = Device(ip, port)
-        self.timeout = 30 #TODO: check this value
+        self._amc = AMCDevice(ip, port)
+        self._axes = {
+            "x": AMC300Axis(self._amc, xyz[0], x_limits, timeout),
+            "y": AMC300Axis(self._amc, xyz[1], y_limits, timeout),
+            "z": AMC300Axis(self._amc, xyz[2], z_limits, timeout)
+        }
+        self.connect()
         
-
     def connect(self):
-        self.amc.connect()
+        self._amc.connect()
 
     def close(self):
-        self.amc.close()
+        self._amc.close()
 
 
-    def move_to(self, axis, position_um):
+    @property
+    def axes(self):
+        # Mapping from string axis names to their corresponding hardware indices
+        #return {name: self._amc.move.getAxis(index) for name, index in self._axis_map.items()}
+        return self._axes
+
+    def may_move_on_enable(self) -> bool:
+        """Whether calling :func:`enable` is likely to make the stage move.
+
+        Most stages need to be driven to their limits at startup to
+        find a repeatable zero position and sometimes to find their
+        limits as well.  This is typically called "homing".
+
+        Stages that need to "home" differ on how often they need it
+        but they only do it during :func:`enable`.  They may need to
+        move each time `enable` is called, the first time after the
+        `Stage` object has been created, or even only the first time
+        since the device was powered up.
+
+        Note the "*may*" on "may_move_on_enable".  This is because it
+        can be difficult to know for certain if `enable` will cause
+        the stage to home.  Still, knowing that the stage *may* move
+        is essential for safety.  An unexpected movement of the stage,
+        particularly large movements such as moving to the stage
+        limits, can destroy a sample on the stage --- or even worse,
+        it can damage an objective or the stage itself.  When in
+        doubt, implementations should return `True`.
+
+        """
+        return False
+
+    @property
+    def position(self) -> typing.Mapping[str, float]:
+        """Map of axis name to their current position.
+
+        .. code-block:: python
+
+            for name, position in stage.position.items():
+                print(f'{name} axis is at position {position}')
+
+        The units of the position is the same as the ones being
+        currently used for the absolute move (:func:`move_to`)
+        operations.
+        """
+        return {name: axis.position for name, axis in self._axes.items()}
+
+    def limits(self) -> typing.Mapping[str, microscope.AxisLimits]:
+        """Map of axis name to its upper and lower limits.
+
+        .. code-block:: python
+
+            for name, limits in stage.limits.items():
+                print(f'{name} axis lower limit is {limits.lower}')
+                print(f'{name} axis upper limit is {limits.upper}')
+
+        These are the limits currently imposed by the device or
+        underlying software and may change over the time of the
+        `StageDevice` object.
+
+        The units of the limits is the same as the ones being
+        currently used for the move operations.
+
+        """
+        return {name: axis.limits for name, axis in self.axes.items()}
+
+    def move_by(self, delta: typing.Mapping[str, float]) -> None:
+        """Move axes by the corresponding amounts.
+
+        Args:
+            delta: map of axis name to the amount to be moved.
+
+        .. code-block:: python
+
+            # Move 'x' axis by 10.2 units and the y axis by -5 units:
+            stage.move_by({'x': 10.2, 'y': -5})
+
+            # The above is equivalent, but possibly faster than:
+            stage.axes['x'].move_by(10.2)
+            stage.axes['y'].move_by(-5)
+
+        The axes will not move beyond :func:`limits`.  If `delta`
+        would move an axis beyond it limit, no exception is raised.
+        Instead, the stage will move until the axis limit.
+
+        """
+        # TODO: implement a software fallback that moves the
+        # individual axis, for stages that don't have provide
+        # simultaneous move of multiple axes.
+        for axis_name, axis_delta in delta.items():
+            axis = self.axes[axis_name]
+            axis.move_by(axis_delta)
+            axis.wait()
         
-        position_nm = position_um * 1000
 
-        if axis in [0, 1]:
-            if not (100 * 1000 <= position_nm <= 5900 * 1000):
-                print("Position out of permitted range. Command not sent")
-                return
-        elif axis == 2:
-            if not (1000 * 1000 <= position_nm <= 5000 * 1000):
-                print("Position out of permitted range. Command not sent")
-                return
-        else:
-            print("Invalid axis name. Command not sent")
-            return
+    def move_to(self, position: typing.Mapping[str, float]) -> None:
+        """Move axes to the corresponding positions.
+
+        Args:
+            position: map of axis name to the positions to move to.
+
+        .. code-block:: python
+
+            # Move 'x' axis to position 8 and the y axis to position -5.3
+            stage.move_to({'x': 8, 'y': -5.3})
+
+            # The above is equivalent to
+            stage.axes['x'].move_to(8)
+            stage.axes['y'].move_to(-5.3)
+
+        The axes will not move beyond :func:`limits`.  If `positions`
+        is beyond the limits, no exception is raised.  Instead, the
+        stage will move until the axes limit.
+
+        """
+        for axis_name, axis_position in position.items():
+            axis = self.axes[axis_name]
+            axis.move_to(axis_position)
+            axis.wait()
+
+
+    # def move_to(self, axis, position_um):
         
-        # NOTE TO SELF: If setControlTargetPosition and setControlMove require numerical,
-        #               I might need to change it back to int 0, 1, 2 
-        self.amc.move.setControlTargetPosition(axis, position_nm)
-        self.amc.control.setControlMove(axis, True)
+    #     position_nm = position_um * 1000
 
-        # Check if the axis starts moving within the timeout period
-        if not self.get_moving_status(axis):
-            print(f"Timeout: Axis {axis} did not start moving within {self.timeout} seconds")
-            self.amc.control.setControlMove(axis, False) # stop the axis
-            return
-
-        # Get the current time
-        start_time = time.time()
+    #     if axis in [0, 1]:
+    #         if not (100 * 1000 <= position_nm <= 5900 * 1000):
+    #             print("Position out of permitted range. Command not sent")
+    #             return
+    #     elif axis == 2:
+    #         if not (1000 * 1000 <= position_nm <= 5000 * 1000):
+    #             print("Position out of permitted range. Command not sent")
+    #             return
+    #     else:
+    #         print("Invalid axis name. Command not sent")
+    #         return
         
-        # Wait for the axis to reach the target position
-        while not self.amc.status.getStatusTargetRange(axis):
-            # Check for timeout
-            if time.time() - start_time > self.timeout:
-                print(f"Timeout: axis {axis} did not reach the target position within {self.timeout} seconds.")
-                self.amc.control.setControlMove(axis, False)
-                return
+    #     # NOTE TO SELF: If setControlTargetPosition and setControlMove require numerical,
+    #     #               I might need to change it back to int 0, 1, 2 
+    #     self._amc.move.setControlTargetPosition(axis, position_nm)
+    #     self._amc.control.setControlMove(axis, True)
+
+    #     # Check if the axis starts moving within the timeout period
+    #     if not self.get_moving_status(axis):
+    #         print(f"Timeout: Axis {axis} did not start moving within {self.timeout} seconds")
+    #         self._amc.control.setControlMove(axis, False) # stop the axis
+    #         return
+
+    #     # Get the current time
+    #     start_time = time.time()
+        
+    #     # Wait for the axis to reach the target position
+    #     while not self._amc.status.getStatusTargetRange(axis):
+    #         # Check for timeout
+    #         if time.time() - start_time > self.timeout:
+    #             print(f"Timeout: axis {axis} did not reach the target position within {self.timeout} seconds.")
+    #             self._amc.control.setControlMove(axis, False)
+    #             return
             
-            # Read out position in nm
-            current_position = self.amc.move.getPosition(axis)
-            print(current_position)
-            time.sleep(0.1)
+    #         # Read out position in nm
+    #         current_position = self._amc.move.getPosition(axis)
+    #         print(current_position)
+    #         time.sleep(0.1)
 
-        # Stop approach
-        self.amc.control.setControlMove(axis, False)
-        print(f"Axis {axis} reached the target position successfully")
+    #     # Stop approach
+    #     self._amc.control.setControlMove(axis, False)
+    #     print(f"Axis {axis} reached the target position successfully")
 
-    # Implementing this function to be compatible with python-microscope structure
-    def get_position(self, axis):
-        # Retrieve the current position of the specified axis
-        current_position_nm = self.amc.move.getPosition(axis)
-        current_position_um = current_position_nm / 1000.0
-        return current_position_um
+    # # Implementing this function to be compatible with python-microscope structure
+    # def get_position(self, axis):
+    #     # Retrieve the current position of the specified axis
+    #     current_position_nm = self._amc.move.getPosition(axis)
+    #     current_position_um = current_position_nm / 1000.0
+    #     return current_position_um
 
-    def get_moving_status(self, axis):
-        start_time = time.time()
+    # def get_moving_status(self, axis):
+    #     start_time = time.time()
 
-        while True:
-            # Call getStatusMoving method to retrieve the moving status of all axes
-            moving_status = self.amc.control.getStatusMovingAllAxes()
+    #     while True:
+    #         # Call getStatusMoving method to retrieve the moving status of all axes
+    #         moving_status = self._amc.control.getStatusMovingAllAxes()
 
-            # Print status for debugging
-            print("Moving Status:", moving_status)
+    #         # Print status for debugging
+    #         print("Moving Status:", moving_status)
 
-            # Check if any axis is moving
-            if any(moving_status):
-                print("At least one axis is moving")
-                return True
+    #         # Check if any axis is moving
+    #         if any(moving_status):
+    #             print("At least one axis is moving")
+    #             return True
             
-            # Check for timeout
-            if time.time() - start_time > self.timeout:
-                print("Timeout: No axis is moving within", self.timeout, "seconds")
-                return False
+    #         # Check for timeout
+    #         if time.time() - start_time > self.timeout:
+    #             print("Timeout: No axis is moving within", self.timeout, "seconds")
+    #             return False
             
-            # Add short delay before checking again
-            time.sleep(0.1)
+    #         # Add short delay before checking again
+    #         time.sleep(0.1)
 
-    def rough_jog(self, axis, direction):
-        if axis == 0 or axis == 1:
-            distance = 100  #TODO: ask Tom/Benji about sensible values
-            if direction == "positive":
-                self.move_to(axis, self.amc.move.getPosition(axis) + distance)
-            elif direction == "negative":
-                self.move_to(axis, self.amc.move.getPosition(axis) - distance)
-            else:
-                print("Invalid direction. Please specify 'positive' or 'negative' for axis 0 and 1")
-        elif axis == 2:
-            distance = 50 #TODO: ask Tom/Benji about sensible values
-            if direction == "positive":
-                self.move_to(axis, self.amc.move.getPosition(axis) + distance)
-            elif direction == "negative":
-                self.move_to(axis, self.amc.move.getPosition(axis) - distance)
-            else:
-                print("Invalid axis. Please specify axis 0, 1 or 2")
+    # def rough_jog(self, axis, direction):
+    #     if axis == 0 or axis == 1:
+    #         distance = 100  #TODO: ask Tom/Benji about sensible values
+    #         if direction == "positive":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) + distance)
+    #         elif direction == "negative":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) - distance)
+    #         else:
+    #             print("Invalid direction. Please specify 'positive' or 'negative' for axis 0 and 1")
+    #     elif axis == 2:
+    #         distance = 50 #TODO: ask Tom/Benji about sensible values
+    #         if direction == "positive":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) + distance)
+    #         elif direction == "negative":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) - distance)
+    #         else:
+    #             print("Invalid axis. Please specify axis 0, 1 or 2")
 
-    def fine_jog(self, axis, direction):
-        if axis == 0 or axis == 1:
-            distance = 10 #TODO: ask Tom/Benji about sensible values
-            if direction == "positive":
-                self.move_to(axis, self.amc.move.getPosition(axis) + distance)
-            elif direction == "negative":
-                self.move_to(axis, self.amc.move.getPosition(axis) - distance)
-            else:
-                print("Invalid direction. Please specify a 'positive' or 'negative' direction")
-        elif axis == 2:
-            distance = 5 #TODO: ask Tom/Benji about sensible values
-            if direction == "positive":
-                self.move_to(axis, self.amc.move.getPosition(axis) + distance)
-            elif direction == "negative":
-                self.move_to(axis, self.amc.move.getPosition(axis) - distance)
-            else:
-                print("invalid axis. Please specify axis 0, 1 or 2")
+    # def fine_jog(self, axis, direction):
+    #     if axis == 0 or axis == 1:
+    #         distance = 10 #TODO: ask Tom/Benji about sensible values
+    #         if direction == "positive":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) + distance)
+    #         elif direction == "negative":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) - distance)
+    #         else:
+    #             print("Invalid direction. Please specify a 'positive' or 'negative' direction")
+    #     elif axis == 2:
+    #         distance = 5 #TODO: ask Tom/Benji about sensible values
+    #         if direction == "positive":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) + distance)
+    #         elif direction == "negative":
+    #             self.move_to(axis, self._amc.move.getPosition(axis) - distance)
+    #         else:
+    #             print("invalid axis. Please specify axis 0, 1 or 2")
 
     def set_frequency(self, axis, frequency):
         if not (3 <= frequency <= 5000):
             print("Frequency out of permitted range. Command not sent")
             return 
-        if self.amc.control.setControlFrequency(axis, frequency):
+        if self._amc.control.setControlFrequency(axis, frequency):
             print(f"Frequency set successfully for axis {axis}")
         else:
             print(f"Failed to set frequency fpr axis {axis}")
@@ -150,22 +307,22 @@ class AMC300Adapter(microscope.abc.Stage):
         if not (0 <= amplitude <= 60):
             print("Amplitude out of permitted range. Command not sent")
             return 
-        if self.amc.control.setControlAmplitude(axis, amplitude):
+        if self._amc.control.setControlAmplitude(axis, amplitude):
             print(f"Amplitude set successfully for axis {axis}")
         else:
             print(f"Failed to set amplitude for axis {axis}")
 
     def get_frequency(self, axis):
-        return self.amc.control.getControlFrequency(axis)
+        return self._amc.control.getControlFrequency(axis)
 
     def get_pos_and_freq(self):    #, axis, position, frequency):
-        return self.amc.control.getPositionsAndVoltages()
+        return self._amc.control.getPositionsAndVoltages()
 
     def get_amplitude(self, axis):
-        return self.amc.control.getControlAmplitude(axis)
+        return self._amc.control.getControlAmplitude(axis)
 
     def get_piezo_amplitude(self, axis):
-        return self.amc.control.getCurrentOutputVoltage(axis)
+        return self._amc.control.getCurrentOutputVoltage(axis)
 
     def home(self):
         self.move_to(0, 3984.1)
@@ -174,13 +331,7 @@ class AMC300Adapter(microscope.abc.Stage):
         print("Axes homed successfully.")
 
     def _do_shutdown(self) -> None:
-        controller.close()
-
-    #@property
-    def axes(self):
-        # Mapping from string axis names to their corresponding hardware indices
-        #return {name: self.amc.move.getAxis(index) for name, index in self._axis_map.items()}
-        pass
+        self.close()
 
     def move_by(self, axis, distance):
         pass
