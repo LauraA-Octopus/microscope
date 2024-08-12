a117d0a0995520c7156813417a4a8376e8b4a871
Laura Arcidiacono <laura.arcidiacono@stfc.ac.uk>
2024-08-12 16:41:38 +0100

    adding decorator for exception handling

--- microscope/lights/HubCwave.py
+++ microscope/lights/HubCwave.py
@@ -183,7 +183,6 @@ class HubCwave(microscope.abc.LightSource):
         self.cwave.set_mirror(position)
         state = "position 1" if position else "position 0"
         _logger.info(f"Set mirror to {state}")
-        
 
     def status_summary(self):
         return self.cwave.get_log()
@@ -192,101 +191,3 @@ class HubCwave(microscope.abc.LightSource):
 if __name__ == "__main__":
     controller = HubCwave(address="192.168.0.4",port= 10001)
     controller.enable()
-    #controller.process_user_input()
-    
-
-
-# My first implementation of the class
-""" class HubnerCwave(microscope.abc.LightSource):
-    # Cwave tunable lasers offer several tuning mechanisms, 
-    # ranging from a fully automated wavelength approach 
-    # to truly continuous mode-hop free scans over tens of GHz
-
-    def __init__(self, ip='192.168.0.4', port=10001):
-        self.ip = ip
-        self.port = port
-        self.cwave = CWave(ip, port)
-        self.cwave.connect()
-        _logger.info(f"Laser started at {self.ip}:{self.port}")
-
-    def connect(self):
-        self.cwave.connect()
-
-    def disconnect(self):
-        self.cwave.disconnect()
-
-    def set_initial_mode(self, mode):
-        if mode =="VIS":
-            self.cwave.set_shutter(ShutterChannel.LaserOut, True) 
-            self.cwave.set_shutter(ShutterChannel.OpoOut, False)
-        elif mode =="IR":
-            self.cwave.set_shutter(ShutterChannel.OpoOut, True)
-            self.cwave.set_shutter(ShutterChannel.LaserOut, False)
-        _logger.info(f"Initial mode set to {mode}.")
-
-    def choose_mode(self):
-        mode = input("Choose mode (VIS or IR): ").strip().upper()
-        if mode not in ["VIS", "IR"]:
-            print("Invalid mode. Please choose 'VIS' or 'IR'")
-            return
-        self.set_initial_mode(self.cwave, mode)
-
-    def change_wavelength(self, wavelength):
-        self.cwave.dial(wavelength, request_shg=False)
-        while not self.cwave.get_dial_done():
-            _logger.info("Waiting for dial operation to complete...")
-            time.sleep(20)
-        _logger.info(f"Wavelength changed to {wavelength} nm")
-
-    def input_wavelength(self):
-        while True:
-            try:
-                wavelength = float(input("Enter the desired wavelength in nm: ").strip())
-                self.change_wavelength(self.cwave, wavelength)
-                break
-            except ValueError:
-                print("Invalid input. Please enter a numeric value.")
-
-    def process_user_input(self):
-        while True:
-            print("\nCommands:")
-            print("1. Choose between VIS or IR")
-            print("2. Input wavelength in nm")
-            print("3. Exit")
-
-            choice = input("Enter your choice: ")
-
-            if choice == "1":
-                self.choose_mode()
-
-            elif choice == "2":
-                self.input_wavelength()
-
-            elif choice == "3":
-                break
-
-            else:
-                print("Invalid choice. Please try again")
-
-    def get_temperature_setpoint(self, channel: TemperatureChannel) -> float:
-        '''Gets temperature setpoint'''
-        assert isinstance(channel, TemperatureChannel)
-        temperature_setpoint = float(self.cwave.__query('t{}_set?'.format(channel.value)))/1000
-        _logger.debug(f"Temperature setpoint for channel {channel.value}: {temperature_setpoint}")
-        return temperature_setpoint
-
-    def get_mapping_temperature(self, channel: MappingChannel, wavelength: float) -> float:
-        '''Gets corresponding temperature of a wavelength according to mapping'''
-        assert isinstance(wavelength, (int, float))
-        mapping_temperature = float(
-            self.cwave.__query('mapping_{}?{}'.format(channel.value, int(wavelength*100))).split(':')[1]
-        )/1000
-        _logger.debug(f"Mapping temperature for channel {channel.value} at wavelength {wavelength}: {mapping_temperature}")
-        return mapping_temperature
-
-    def get_opoLock_status(self) -> bool:
-        '''Gets the OpoLock status'''
-        status = self.cwave.opoLock()
-        _logger.debug(f"OpoLock status: {status}")
-        return status
- """
