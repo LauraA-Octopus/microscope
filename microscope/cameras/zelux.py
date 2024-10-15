a72965ba3e54c5df52976cf6fe5ba5b76c9dc11e
LauraA-Octopus <laura.arcidiacono@stfc.ac.uk>
2024-10-15 14:50:13 +0100

    to compare with sdk polling

--- microscope/cameras/zelux.py
+++ microscope/cameras/zelux.py
@@ -1,43 +1,163 @@
 import numpy as np
-from thorlabs_tsi_sdk.tl_camera import TLCameraSDK, OPERATION_MODE
+#import cv2
+import microscope
+import microscope.abc
+from microscope.cameras.SDK.Python_Toolkit.thorlabs_tsi_sdk.thorlabs_tsi_sdkpy.tl_camera import TLCameraSDK, OPERATION_MODE, TRIGGER_POLARITY
 
 NUM_FRAMES = 10
 
-with TLCameraSDK as sdk:
-    available_cameras = sdk.discover_available_cameras()
-    if len(available_cameras) < 1:
-        print("No cameras detected")
+### ask Benji to have only one zelux switched on #####
 
-    with sdk.open_camera(available_cameras[0]) as camera:
-        camera.exposure_time_us = 11000
-        # start the camera in continuous mode
-        camera.frames_per_triffer_zero_for_unlimited = 0 
-        # 1s polling timeout
-        camera.image_poll_timeout_ms = 1000
-        # store the current ROI
-        old_roi = camera.roi
+class ZeluxCamera(microscope.abc.Camera):
+    def __init__(self):
+        self._fetch_thread = None
+        self.zelux = TLCameraSDK()
+        available_cameras = self.zelux.discover_available_cameras()
+        if len(available_cameras) < 1:
+            raise RuntimeError("No cameras detected")
 
-        camera.arm(2)
+        self.camera = self.zelux.open_camera(available_cameras)
+        if self.camera is None:
+            raise RuntimeError("Failed to open camera")
 
-        # Uncomment if we want the camera software triggered
-        # camera.issue_software_trigger()
+        print(f"Camera serial number: {self.camera.serial_number}")
+        print(f"hdw trigger rising edge 0, falling edge 1: {self.camera.trigger_polarity}")
+        self.camera.exposure_time_us = 11000
+        self.camera.frames_per_trigger_zero_for_unlimited = 0
+        self.camera.image_poll_timeout_ms = 30000
+        self.old_roi = self.camera.roi 
 
-        for i in range(NUM_FRAMES):
-            frame = camera.get_pending_frame_or_null()
-            if frame is not None:
-                print("frame #{} received!".format(frame.frame_count))
+    def _do_shutdown(self):
+        if self.camera and self.camera.is_armed:
+            self.camera.disarm()
+        if self.camera:
+            self.camera.dispose()
+        self.zelux.dispose()
 
-                frame.image_buffer
-                #NOTE: the above is a temporary memory buffer
-                # that may be overwritten during the next call
-                # to get_pending_frame_or_null the following line
-                # makes a deep copy of the image data:
-                image_buffer_copy = np.copy(frame.image_buffer)
+    def _do_trigger(self):
+        self.camera.issue_software_trigger()
+        #self.camera.issue_hardware_trigger()
+
+    def _fetch_data(self):
+        frame = self.camera.get_pending_frame_or_null()
+        if frame:
+            return np.copy(frame.image_buffer), frame.frame_count
+        return None
+
+    def _get_binning(self):
+        return self.camera.biny_range
+
+    def _set_binning(self, binx, biny): 
+        return self.camera.biny,  self.camera.binx
+
+    def _get_roi(self):
+        return self.camera.roi
+
+    def _set_roi(self, roi):
+        self.camera.roi = roi
+
+    def _get_sensor_shape(self):
+        # Return sensor shape width and hight
+        return (self.camera.sensor_width_pixels, self.camera.sensor_height_pixels)
+
+    def abort(self):
+        # Abort any ongoing acquisition
+        self.camera.disarm()
+
+    def set_exposure_time(self, exposure_time):
+        # Set the camera exposure time in seconds
+        self.camera.exposure_time_us = 50000     #int(exposure_time * 1e6)
+
+    def set_trigger(self, trigger_type, trigger_mode):
+        # Set the camera's trigger type and mode
+        if trigger_type == 'hardware':
+            self.camera.operation_mode = OPERATION_MODE.HARDWARE_TRIGGERED
+            self.camera.trigger_polarity = TRIGGER_POLARITY.ACTIVE_HIGH
+        else:
+            self.camera.operation_mode = OPERATION_MODE.SOFTWARE_TRIGGERED
+
+    def trigger_mode(self):
+        return self.camera.operation_mode
+
+    def trigger_type(self):
+        if self.camera.operation_mode == OPERATION_MODE.HARDWARE_TRIGGERED:
+            return 'hardware'
+        elif self.camera.operation_mode == OPERATION_MODE.SOFTWARE_TRIGGERED:
+            return 'software'
+
+    def start_acquisition(self):
+        print("Starting acquisition")
+        self.camera.arm(2)
+        print("Camera is armed: ", self.camera.is_armed)
+
+    def stop_acquisition(self):
+        self.camera.disarm()
+
+    def _fetch_data_continuously(self):
+        frame_count = 0
+        while self._fetch_thread is not None:
+            # Fetch the frame
+            frame_data, count = self._fetch_data()
+
+            if frame_data is not None:
+                frame_count += 1
+
+                # Display the frame live
+                #cv2.imshow("Live Camera Feed", frame_data)
+                
+                # Save frames
+                file_name = f"frame_{frame_count}.png"
+                #cv2.imwrite(file_name, frame_data)
+                print(f"Frame #{count} displayed and saved as {file_name}")
+
+                # Press q to exit live display
+                #if cv2.waitKey(1) & 0xFF == ord('q'):
+                    #break 
+
             else:
-                print("Timeout reached during polling, program exiting...")
-                break
-        camera.disarm()
-        # Reset the ROI back to the original ROI
-        camera.roi = old_roi
+                print("No frame received or timeout reached")
 
-print("program completed")  
+        #cv2.destroyAllWindows()
+
+    def grab_frame(self):
+        if not self.camera.is_armed:
+            print("Camera is not armed. Arming now...")
+            self.camera.arm(2)
+        print("camera is armed: ", self.camera.is_armed)
+        
+        for _ in range(5):
+            frame = self.camera.get_pending_frame_or_null()
+            if frame is not None:
+                print(f"Frame #{frame.frame_count} received")
+                # Make a deep copy of the image buffer
+                return np.copy(frame.image_buffer)
+            else:
+                print("No frames received, retrying...")
+            print("Timeout reached during polling")
+            return None
+
+    def acquire_images(self, num_frames=NUM_FRAMES):
+        # Acquire multiple frames from the camera
+        #self.start_acquisition()
+        for i in range(num_frames):
+            image = self.grab_frame()
+            if image is None:
+                print(f"No frames received for {i+1}")
+            else:
+                print(f"Frame {i+1} acquired")
+        self.stop_acquisition()
+    
+    def close(self):
+        if self.camera.is_armed:
+            self.camera.disarm()
+        # Close the camera and SDK when done
+        self.camera.dispose()
+        self.zelux.dispose()
+
+#camera = ZeluxCamera()
+try:
+    ZeluxCamera.acquire_images(NUM_FRAMES)
+#cam.set_trigger('hardware', 'rising_edge')
+    ZeluxCamera.set_trigger('software', None)
+finally:
+    ZeluxCamera.close
